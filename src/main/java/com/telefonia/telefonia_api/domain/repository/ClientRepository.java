a29b484fd2f6f0466b715997f6122724419c1d1d
illancapan <igorllancapan@gmail.com>
2025-02-04 19:01:09 -0300

    Desarrollo desde 0

--- src/main/java/com/telefonia/telefonia_api/domain/repository/ClientRepository.java
+++ src/main/java/com/telefonia/telefonia_api/domain/repository/ClientRepository.java
@@ -4,6 +4,4 @@ import com.telefonia.telefonia_api.domain.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
-
 }
--- src/main/java/com/telefonia/telefonia_api/domain/repository/ContractRepository.java
+++ src/main/java/com/telefonia/telefonia_api/domain/repository/ContractRepository.java
@@ -3,5 +3,5 @@ package com.telefonia.telefonia_api.domain.repository;
 import com.telefonia.telefonia_api.domain.model.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContractRepository extends JpaRepository<Contract,Long> {
+public interface ContractRepository extends JpaRepository<Contract, Long> {
 }
